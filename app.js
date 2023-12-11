9f3d6335c4d5a223e9662de4d4339f1631f794b7
Laraagr1 <laraagrela94@gmail.com>
2023-12-11 14:53:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const port = 7000
+
+var basqueteRouter = require('./routes/basquete');
+var cadastroRouter = require('./routes/cadastro');
+var corridaRouter = require('./routes/corrida');
+var futebolRouter = require('./routes/futebol');
+var loginRouter = require('./routes/login');
+var lojaRouter = require('./routes/loja');
+var produtoRouter = require('./routes/produto');
+var skateRouter = require('./routes/skate');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/basquete', basqueteRouter);
+app.use('/cadastro', cadastroRouter);
+app.use('/corrida', corridaRouter);
+app.use('/futebol', futebolRouter);
+app.use('/login', loginRouter);
+app.use('/', lojaRouter);
+app.use('/produto', produtoRouter);
+app.use('/skate', skateRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+app.listen(7000, () => {
+    console.log("rodando")
+})
